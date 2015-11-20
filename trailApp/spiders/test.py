3d109e79c203d443989e0c5eb74f0df5b813b58b
nagatak <nagatak@hawaii.edu>
2015-11-19 18:32:40 -1000

    updated spider test.py for testing

--- trailApp/spiders/test.py
+++ trailApp/spiders/test.py
@@ -5,18 +5,23 @@ from trailApp.items import TrailappItem
 
 class HavoSpider(scrapy.Spider):
     name = "test"
-    allowed_domains = ["nps.gov"]
+    #allowed_domains = ["*"]
     start_urls = (
-    'http://www.nps.gov/pwr/renderhandlers/cs_chooser/rh_alert_chooser_json.cfm?siteCode=havo&ts=',
+        #'http://www.nps.gov/havo/nps-alerts-havo.json?tmsp=',
+        'file:////Users//Mac//Desktop//cs294//scrapy//trailApp//havoAlert.json',
     )
 
     def parse(self, response):
         items = []
         jsonresponse = json.loads(response.body_as_unicode())
-        for sel in jsonresponse["items"]:
+        for sel in jsonresponse["CEDATA"]:
             item = TrailappItem()
-            item["title"] = sel ["title"]
-            item["description"] = sel ["description"]
+            item["title"] =  sel["FIC_title"]
+            item["description"] = sel["FIC_description"]
             items.append(item)
 
         yield item
+   # def parse(self, response):
+   #     filename = 'test.html'
+   #     with open(filename, 'wb') as f:
+   #         f.write(response.body)
